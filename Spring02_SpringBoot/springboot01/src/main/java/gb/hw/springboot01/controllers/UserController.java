bd446ebbec640e7f22a4af3716fcf27f8dde0885
kvo52@kvo1 <kvo.52@mail.ru>
2024-01-17 21:29:55 +0300

    HW02_SpringBoot_structures

---
+++ Spring02_SpringBoot/springboot01/src/main/java/gb/hw/springboot01/controllers/UserController.java
@@ -0,0 +1,44 @@
+package gb.hw.springboot01.controllers;
+
+import gb.hw.springboot01.model.User;
+import gb.hw.springboot01.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model){
+        List<User> users = userService.findAll();
+
+
+        model.addAttribute("users", users);
+        return "user-list";
+        //return "home.html";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    //@GetMapping("user-delete/{id}")
+}
---
+++ Spring02_SpringBoot/springboot01/src/test/java/gb/hw/springboot01/Springboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.hw.springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
