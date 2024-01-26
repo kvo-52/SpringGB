705956cf2fcef7dcbc91c076092555bd59f1138d
kvo52@kvo1 <kvo.52@mail.ru>
2024-01-26 14:37:39 +0300

    HW03_RESTApi_structurs

---
+++ Spring03_RESTApp/AppHW03/src/test/java/ru/gb/AppHW03/AppHw03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.AppHW03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHw03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
