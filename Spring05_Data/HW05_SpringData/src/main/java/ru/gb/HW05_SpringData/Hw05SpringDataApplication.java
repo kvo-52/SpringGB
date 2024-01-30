4864c4887032ca2187445b9b85548cdaaf49d323
kvo52@kvo1 <kvo.52@mail.ru>
2024-01-30 18:56:47 +0300

    HW05_SpringData_structuries project

---
+++ Spring05_Data/HW05_SpringData/src/main/java/ru/gb/HW05_SpringData/Hw05SpringDataApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.HW05_SpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw05SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw05SpringDataApplication.class, args);
+	}
+
+}
---
+++ Spring05_Data/HW05_SpringData/src/test/java/ru/gb/HW05_SpringData/Hw05SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.HW05_SpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw05SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
