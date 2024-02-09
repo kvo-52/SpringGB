fafcff4b2f25e1ba1700225dd2f543f9fbc25ce4
kvo52@kvo1 <kvo.52@mail.ru>
2024-02-09 15:19:16 +0300

    hw07_Security_new

---
+++ Spring07_Security/hw07Security/src/main/java/ru/gb/hw07Security/Hw07SecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.hw07Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw07SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw07SecurityApplication.class, args);
+	}
+
+}
---
+++ Spring07_Security/hw07Security/src/test/java/ru/gb/hw07Security/Hw07SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.hw07Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw07SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
